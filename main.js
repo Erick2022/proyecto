818091406e24c319fd71cd067c07b660b1c0f915
Erick2022 <96039254+Erick2022@users.noreply.github.com>
2022-09-28 21:48:26 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function selectHD() {
+    let selects = document.getElementById('selecciones');
+    let horainspected = selects.value;
+
+    document.getElementById('selecciones').innerHTML = horainspected
+}
+let fechaActual = new Date();
+
+function fechaActual() {
+    let fechaActual = new Date();
+    document.getElementById('fechaActual').innerHTML = fechaActual
+}
+
+
+
